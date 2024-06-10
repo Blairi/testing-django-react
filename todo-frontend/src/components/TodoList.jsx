e3051352bc7b1d2eced28beac34fb58e53abfef8
Blairi <axel.avi.fer@gmail.com>
2024-06-10 14:22:47 -0600

    todo list

---
+++ todo-frontend/src/components/TodoList.jsx
@@ -0,0 +1,13 @@
+import { TodoListItem } from "./TodoListItem"
+
+export const TodoList = () => {
+  return (
+    <ul className="grid gap-3 md:grid-cols-3">
+      {
+        // [1,1,1,1,1,1,1,1,1,1,1,1,1].map((item, index) => (
+        //   <TodoListItem />
+        // ))
+      }
+    </ul>
+  )
+}
