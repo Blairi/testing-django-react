f70b6aece6d678386e64a3e65982b5009dfaacdc
Blairi <axel.avi.fer@gmail.com>
2024-06-19 17:00:48 -0600

    api docs

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
+
 from rest_framework import routers
+from rest_framework.documentation import include_docs_urls
+
 from tasks import views
 
 router = routers.DefaultRouter()
@@ -7,5 +10,6 @@ router.register('tasks', views.TaskView, 'view')
 
 # api versioning
 urlpatterns = [
-    path('api/v1/', include(router.urls))
+    path('api/v1/', include(router.urls)),
+    path('docs/', include_docs_urls(title="Tasks API"))
 ]
